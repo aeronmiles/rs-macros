e6301072d7628e6ececa2ea9b2f192dc0aa09441
Aeron <aeron.miles@diverseinteractive.com>
2021-01-02 16:28:55 +0000

    macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,12 +1,5 @@
 #![allow( unused_macros)]
 
-// #[macro_export]
-// macro_rules! assert_prntln_eq {
-//     () => {
-        
-//     };
-// }
-// assert_eq!(ws.is_err(), false);
 
 #[macro_export]
 macro_rules! test_async_tokio {
